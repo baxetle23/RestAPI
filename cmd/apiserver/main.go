4bc9c9c5b4ed16a062f379e3fad9b90e5c1ef2b2
Aleksei Ponyatov <ponyatov94@gmail.com>
2023-01-15 18:01:51 +0300

    first commit

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"github.com/BurntSushi/toml"
+	"httpRestAPI/internal/app/apiserver"
+	"log"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to config file")
+}
+
+func main() {
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	if _, err := toml.DecodeFile(configPath, config); err != nil {
+		log.Fatal(err)
+	}
+
+	s := apiserver.New(config)
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
